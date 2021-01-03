428c6e2c0508da83037f88167dd50368b860c9fd
xorlickyl <45417555+xorlickyl@users.noreply.github.com>
2021-01-03 21:55:18 +0100

    update

--- src/main.js
+++ src/main.js
@@ -34,16 +34,39 @@ const {ipcMain} = require('electron')
 // receive message from index.html
 ipcMain.on('url',function (event, arg) {
     console.log( arg );
+    var url = arg.substring(8);
+    url=url.replace("/","-");
+    console.log(url);
     const  req = net.request({
         method:'GET',
         protocol:'http:',
         host:'127.0.0.1:5000',
-        path:'/elements'
+        path:'/elements/'+url
     });
     req.on('response',(response) => {
-    console.log(response.statusCode)}
-    );
-    req.setHeader('url',arg);
+        console.log(response.statusCode)
+        console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+
+        response.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`)
+            event.sender.send("finished",chunk.toString());
+        });
+    });
+    req.on('finish', () => {
+        console.log('Request is Finished')
+    });
+    req.on('abort', () => {
+        console.log('Request is Aborted')
+    });
+    req.on('error', (error) => {
+        console.log(`ERROR: ${JSON.stringify(error)}`)
+    });
+    req.on('close', (error) => {
+        console.log('Last Transaction has occured')
+    });
+    console.log(req);
+    //req.write(url,'utf-8');
+    req.end();
    // event.sender.send('finish',arg);
     // send message to index.html
    // event.sender.send('asynchronous-reply', arg);
